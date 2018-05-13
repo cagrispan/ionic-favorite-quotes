2aae2bdd1a7b38bcae571fb8c4c61f2e8f35fb33
Carlos Augusto Grispan <cagrispan@gmail.com>
2018-05-13 12:11:55 -0300

    Side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-import { Platform } from 'ionic-angular';
+import { MenuController, NavController, Platform } from 'ionic-angular';
 
+import { SettingsPage } from '../pages/settings/settings';
 import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  tabsPage: any = TabsPage;
+  settingsPage: any = SettingsPage;
+  @ViewChild('nav') nav: NavController;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    private menuController: MenuController
+  ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  onLoad(page: any) {
+    this.nav.setRoot(page);
+    this.menuController.close();
   }
 }
 
